ad39e968707eda495853671eebdef1ad046bb836
rehemajaffar <rehemajumajaffar@gmail.com>
2022-02-11 21:54:43 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var  name ="Rehema"
+    println(name)
+    var age ="21"
+    println(age)
+    var number ="0707917602"
+    println(number)
+    var weight ="78kg"
+    println(weight)
+    var country ="kenya"
+    println(country)
+
+}
