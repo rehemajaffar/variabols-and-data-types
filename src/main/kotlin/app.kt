0e173889ffceee3b5d3432c109c7041570bcd7b9
rehemajaffar <99390203+rehemajaffar@users.noreply.github.com>
2022-02-12 23:27:54 +0300

    Update app.kt

--- src/main/kotlin/app.kt
+++ src/main/kotlin/app.kt
@@ -1,18 +1,31 @@
 fun main() {
-    var  name ="Rehema juma"
-    println(name)
-    var age ="21"
-    println(age)
-    var number ="0707917602"
-    println(number)
-    var weight ="78kg"
-    println(weight)
-    var country ="kenya"
-    println(country)
+    var  name = arrayOf("Rehema juma", "Asia jaffar", "Ismail asia", "Mwangi kimau")
+    println(name[0])
+    var age = arrayOf("21", "45", "89", "84")
+    println(age[0])
+    var number = arrayOf("0707917602", "0798453284", "0789765432", "0756748329")
+    println(number[0])
+    var weight = arrayOf("78kg", "98", "30", "45")
+    println(weight[0])
+    var Kenyan= arrayOf("false", "true", "false", "true")
+    println(Kenyan[0])
 
-    var iskenya = "true"
-    var isnotkenya = "false"
-    println("kenya")
-    println("notkenya")
+    println(name[1])
+    println(age[1])
+    println(number[1])
+    println(weight[1])
+    println(Kenyan[1])
+
+    println(name[2])
+    println(age[2])
+    println(number[2])
+    println(weight[2])
+    println(Kenyan[2])
+
+    println(name[3])
+    println(age[3])
+    println(number[3])
+    println(weight[3])
+    println(Kenyan[3])
 
 }
